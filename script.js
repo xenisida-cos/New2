8d4d73693e817c7deb53c2d0a447b64a31b03efe
KloVis <xenisida@gmail.com>
2026-09-11 20:32:48 +0300

    HM
    HomeWork JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function showData() {
+    let name = 
+    document.getElementById("name").value;
+    let surename =
+    document.getElementById("surename").value;
+}
+
+alert(
+    "Your name = " + name + " " + surename
+)
